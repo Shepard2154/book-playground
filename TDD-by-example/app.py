3cf087266bc4091b308ac60ee12cac77439239aa
Oleg Burnaev <john-shepard-2154@yandex.ru>
2026-07-22 21:22:07 +0300

    wip: remove duplication (page 46 of the book)

--- TDD-by-example/app.py
+++ TDD-by-example/app.py
@@ -1,22 +1,17 @@
 class Money:
-    _amount = None
+    def __init__(self, amount):
+        self._amount = amount
 
     def __eq__(self, other):
         return self._amount == other
 
-
-class Dollar(Money):
-    def __init__(self, amount):
-        self._amount = amount
-
     def __mul__(self, multiplier):
         return self._amount * multiplier
+
+
+class Dollar(Money):
+    pass
 
 
 class Franc(Money):
-    def __init__(self, amount):
-        self._amount = amount
-
-    def __mul__(self, multiplier):
-        return self._amount * multiplier
-
+    pass
