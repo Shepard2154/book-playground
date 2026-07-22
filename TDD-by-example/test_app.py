da17664b6f14ecad44d34aa3d189b6b7591ebfff
Oleg Burnaev <john-shepard-2154@yandex.ru>
2026-07-22 20:51:32 +0300

    green test: polish code (page 37 of the book)

--- TDD-by-example/test_app.py
+++ TDD-by-example/test_app.py
@@ -3,9 +3,10 @@ from app import Dollar
 
 def test_multiplication():
     five = Dollar(5)
-    assert five * 2 == 10
-    assert five * 3 == 15
+    assert five * 2 == Dollar(10)
+    assert five * 3 == Dollar(15)
 
 
 def test_equality():
     assert Dollar(5) == Dollar(5)
+    assert Dollar(5) != Dollar(6)
