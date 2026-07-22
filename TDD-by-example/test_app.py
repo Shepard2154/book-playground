8340a36212a278f33c2684d39315172f982a60f9
Oleg Burnaev <john-shepard-2154@yandex.ru>
2026-07-22 20:30:25 +0300

    red test: equals() (page 33 of the book)

--- TDD-by-example/test_app.py
+++ TDD-by-example/test_app.py
@@ -4,3 +4,8 @@ from app import Dollar
 def test_multiplication():
     five = Dollar(5)
     assert five * 2 == 10
+    assert five * 3 == 15
+
+
+def test_equality():
+    assert Dollar(5) == Dollar(5)
