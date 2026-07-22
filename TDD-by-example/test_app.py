66aa357987490701234aca0301afd2d56e00ccd6
Oleg Burnaev <john-shepard-2154@yandex.ru>
2026-07-22 20:58:22 +0300

    red test: Franc multiplication (page 39 of the book)

--- TDD-by-example/test_app.py
+++ TDD-by-example/test_app.py
@@ -10,3 +10,9 @@ def test_multiplication():
 def test_equality():
     assert Dollar(5) == Dollar(5)
     assert Dollar(5) != Dollar(6)
+
+
+def test_franc_multiplication():
+    five = Franc(5)
+    assert franc * 2 == Franc(10)
+    assert five * 3 == Franc(15)
