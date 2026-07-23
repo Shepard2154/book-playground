e079c8ef1d6c34eee583edae7a7ae14f5cb1bd40
Oleg Burnaev <john-shepard-2154@yandex.ru>
2026-07-23 07:23:09 +0300

    green test: add term currency (about page 62)

--- TDD-by-example/app.py
+++ TDD-by-example/app.py
@@ -1,12 +1,29 @@
 class Money:
-    def __init__(self, amount):
+    def __init__(self, amount, currency):
         self._amount = amount
+        self._currency = currency
 
     def __eq__(self, other):
-        return self._amount == other
+        return self.amount == other.amount and self.currency == other.currency
 
     def __mul__(self, multiplier):
-        return self._amount * multiplier
+        return Money(self.amount * multiplier, self.currency)
+
+    @staticmethod
+    def dollar(amount):
+        return Money(amount, 'USD')
+
+    @staticmethod
+    def franc(amount):
+        return Money(amount, 'CHF')
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @property
+    def currency(self):
+        return self._currency
 
 
 class Dollar(Money):
--- TDD-by-example/test_app.py
+++ TDD-by-example/test_app.py
@@ -1,16 +1,20 @@
-from app import Money, Franc, Money
+from app import Money
 
 
 def test_multiplication():
-    five = Money(5)
-    assert five * 2 == Money(10)
-    assert five * 3 == Money(15)
+    five = Money.dollar(5)
+    assert five * 2 == Money.dollar(10)
+    assert five * 3 == Money.dollar(15)
 
 
 def test_equality():
-    assert Money(5) == Money(5)
-    assert Money(5) != Money(6)
-    assert Franc(5) == Franc(5)
-    assert Franc(5) != Franc(6)
-    assert Franc(5) != Money(5)
+    assert Money.dollar(5) == Money.dollar(5)
+    assert Money.dollar(5) != Money.dollar(6)
+    assert Money.franc(5) == Money.franc(5)
+    assert Money.franc(5) != Money.franc(6)
+    assert Money.franc(5) != Money.dollar(5)
 
+
+def test_currency():
+    assert('USD', Money.dollar(1).currency)
+    assert('CHF', Money.franc(1).currency)
